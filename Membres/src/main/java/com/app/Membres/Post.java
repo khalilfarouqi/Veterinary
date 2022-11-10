b4c42095d7ba042cefd0389199c2c1ead30d78a9
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 14:29:00 +0100

    Members entity enum create commit

---
+++ Membres/src/main/java/com/app/Membres/Post.java
@@ -0,0 +1,5 @@
+package com.app.Membres;
+
+public enum Post {
+    doctor, secretary, nurse, housemaid
+}
---
+++ Membres/src/main/java/com/app/Membres/entity/Members.java
@@ -0,0 +1,37 @@
+package com.app.Membres.entity;
+
+import com.app.Membres.Post;
+import com.fasterxml.jackson.annotation.*;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="Members")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
+public class Members {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    @Column(name = "FirstName")
+    private String FirstName;
+
+    @Column(name = "LastName")
+    private String LastName;
+
+    @Column(name = "BirthDay")
+    private Date BirthDay;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Post")
+    private Post post;
+
+    @Column(name = "HiringDate")
+    private Date Hiring_Date;
+
+}
