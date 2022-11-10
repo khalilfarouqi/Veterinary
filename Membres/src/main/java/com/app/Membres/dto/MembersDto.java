6cd7324d7f16d4edb97956797ffec6b167ece051
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 14:32:32 +0100

    Members dto create commit

--- Membres/src/main/java/com/app/Membres/dto/MembersDto.java
+++ Membres/src/main/java/com/app/Membres/dto/MembersDto.java
@@ -1,4 +1,18 @@
 package com.app.Membres.dto;
 
+import com.app.Membres.Post;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class MembersDto {
+
+    private Long ID;
+    private String FirstName;
+    private String LastName;
+    private Date BirthDay;
+    private Post post;
+    private Date Hiring_Date;
+
 }
