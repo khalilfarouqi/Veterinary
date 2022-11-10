78b16a8edf23d26b6bc5962cf2856a7347e7f019
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 12:29:39 +0100

    Proprietary Controller create commit

--- Animaux/src/main/java/com/app/Animaux/business/rest/controller/ProprietaryController.java
+++ Animaux/src/main/java/com/app/Animaux/business/rest/controller/ProprietaryController.java
@@ -1,9 +1,73 @@
 package com.app.Animaux.business.rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.app.Animaux.business.dto.ProprietaryDto;
+import com.app.Animaux.business.entity.Proprietary;
+import com.app.Animaux.business.service.ProprietaryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/proprietary")
 public class ProprietaryController {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private ProprietaryService proprietaryService;
+
+    private ProprietaryController(ProprietaryService proprietaryService){
+        this.proprietaryService = proprietaryService;
+    }
+
+    @GetMapping
+    public List<ProprietaryDto> getAllProprietary(){
+        return proprietaryService.getAllProprietary().stream().map(proprietary -> modelMapper.map(proprietary, ProprietaryDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProprietaryDto> getProprietaryById(@PathVariable(name = "id") Long id){
+        Proprietary proprietary = proprietaryService.getProprietaryById(id);
+
+        ProprietaryDto proprietaryResponse = modelMapper.map(proprietary, ProprietaryDto.class);
+
+        return ResponseEntity.ok().body(proprietaryResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProprietaryDto> createProprietary(@RequestBody ProprietaryDto proprietaryDto){
+
+        Proprietary proprietaryRequest = modelMapper.map(proprietaryDto, Proprietary.class);
+
+        Proprietary proprietary = proprietaryService.createProprietary(proprietaryRequest);
+
+        ProprietaryDto proprietaryResponse = modelMapper.map(proprietary, ProprietaryDto.class);
+
+        return new ResponseEntity<ProprietaryDto>(proprietaryResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProprietaryDto> updateProprietary(@PathVariable long id, @RequestBody ProprietaryDto proprietaryDto){
+
+        Proprietary proprietaryRequest = modelMapper.map(proprietaryDto, Proprietary.class);
+
+        Proprietary proprietary = proprietaryService.updateProprietary(id, proprietaryRequest);
+
+        ProprietaryDto proprietaryResponse = modelMapper.map(proprietary, ProprietaryDto.class);
+
+        return ResponseEntity.ok().body(proprietaryResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProprietary(@PathVariable(name = "id") Long id){
+        proprietaryService.deleteProprietary(id);
+        return new ResponseEntity<String>("Proprietary deleted successfully", HttpStatus.OK);
+    }
+
 }
