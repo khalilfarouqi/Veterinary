1b0075f354859ea26c406cc2f8fbc623e4d3f89f
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 14:00:12 +0100

    beans mapping commit

--- Medicaments/src/main/java/com/app/Medicaments/MedicamentsApplication.java
+++ Medicaments/src/main/java/com/app/Medicaments/MedicamentsApplication.java
@@ -1,10 +1,17 @@
 package com.app.Medicaments;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MedicamentsApplication {
+
+	@Bean
+	public ModelMapper modelMapper (){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicamentsApplication.class, args);
