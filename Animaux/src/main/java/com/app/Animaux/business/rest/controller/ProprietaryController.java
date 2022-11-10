101a066913485d6227434b434a035b40af024097
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 12:22:55 +0100

    Proprietary Controller commit

--- Animaux/src/main/java/com/app/Animaux/business/rest/controller/ProprietaryController.java
+++ Animaux/src/main/java/com/app/Animaux/business/rest/controller/ProprietaryController.java
@@ -1,4 +1,9 @@
 package com.app.Animaux.business.rest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/proprietary")
 public class ProprietaryController {
 }
