c3210561e1509f8087bd7c6ef8b8599b21d0ac1c
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 12:22:40 +0100

    Medication Controller commit

---
+++ Medicaments/src/main/java/com/app/Medicaments/controller/MedicationController.java
@@ -0,0 +1,9 @@
+package com.app.Medicaments.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/medications")
+public class MedicationController {
+}
