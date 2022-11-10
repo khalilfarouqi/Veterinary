6eb44b5ac2f1ed5b7037850b22a0c0eb5f23afd4
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 11:41:44 +0100

    medication Repository create commit

--- Medicaments/src/main/java/com/app/Medicaments/repository/MedicationRepository.java
+++ Medicaments/src/main/java/com/app/Medicaments/repository/MedicationRepository.java
@@ -2,7 +2,9 @@ package com.app.Medicaments.repository;
 
 import com.app.Medicaments.entity.Medications;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicationRepository extends JpaRepository <Medications, Long> {
 
 }
