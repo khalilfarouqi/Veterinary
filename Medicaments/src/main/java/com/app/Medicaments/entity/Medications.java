a9bf127d5695eff3b1554c562dca6727b8c4e247
khalilfarouqi <bio.khalil.f@gmail.com>
2022-11-10 11:21:45 +0100

    medication entity create commit

---
+++ Medicaments/src/main/java/com/app/Medicaments/entity/Medications.java
@@ -0,0 +1,32 @@
+package com.app.Medicaments.entity;
+
+import com.fasterxml.jackson.annotation.*;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Medications")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
+public class Medications {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    @Column(name = "Nom")
+    private String Nom;
+
+    @Column(name = "Marque")
+    private String Marque;
+
+    @Column(name = "UPET")
+    //UPET = unit price excluding taxes
+    private Double UPET;
+
+    @Column(name = "QteStock")
+    private int QteStock;
+
+}
