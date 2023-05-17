afebdc1106fd13203977d629e07913aa567046d7
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-05-17 16:37:26 +0100

    clean main class

--- Animaux/src/main/java/com/app/Animaux/AnimauxApplication.java
+++ Animaux/src/main/java/com/app/Animaux/AnimauxApplication.java
@@ -3,19 +3,15 @@ package com.app.Animaux;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnimauxApplication {
-
+	public static void main(String[] args) {
+		SpringApplication.run(AnimauxApplication.class, args);
+	}
 	@Bean
 	public ModelMapper modelMapper (){
 		return new ModelMapper();
 	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(AnimauxApplication.class, args);
-	}
-
 }
